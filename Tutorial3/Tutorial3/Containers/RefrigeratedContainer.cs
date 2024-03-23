8c25442164870d772e7a5a12187857b8154efdb9
Maryna Kamienieva <mkdirwd@gmail.com>
2024-03-23 15:10:12 +0100

    Have done class RefrigeratedContainer and ShipContainer

--- Tutorial3/Tutorial3/Containers/RefrigeratedContainer.cs
+++ Tutorial3/Tutorial3/Containers/RefrigeratedContainer.cs
@@ -2,16 +2,49 @@ namespace Tutorial3.Containers;
 
 public class RefrigeratedContainer
 {
-    public string ProductType { get; set; }
+    public PossibleProduct ProductType { get; set; }
     public double TemperatureNow { get; set; }
     public double TemperatureRequiered { get; set; }
 
-    public RefrigeratedContainer(string productType, double temperatureNow,
+    public RefrigeratedContainer(PossibleProduct productType, double temperatureNow,
         double temperatureRequiered)
     {
         ProductType = productType;
-        TemperatureNow = TemperatureRequiered;
+        TemperatureNow = temperatureNow;
         TemperatureRequiered = temperatureRequiered;
+    }
+    
+    private double GetTemperature(PossibleProduct products) {
+        switch (products) {
+            case PossibleProduct.Bananas:
+                return 13.3;
+            case PossibleProduct.Chocolate:
+                return 18;
+            case PossibleProduct.Fish:
+                return 2;
+            case PossibleProduct.Meat:
+                return -15;
+            case PossibleProduct.Ice_cream:
+                return -18;
+            case PossibleProduct.Frozem_pizza:
+                return -30;
+            case PossibleProduct.Cheese:
+                return 7.2;
+            case PossibleProduct.Sausages:
+                return 5;
+            case PossibleProduct.Butter:
+                return 20.5;
+            case PossibleProduct.Eggs:
+                return 19;
+            default:
+                throw new System.Exception("Unknown product.");
+        }
+    }
+    
+    public void SetTemperature(double temperature) {
+        if (temperature < TemperatureRequiered)
+            throw new System.Exception("Error - the temperature of the container cannot be lower than the temperature required by a given type of product.");
+        TemperatureNow = temperature;
     }
     
 }
--- Tutorial3/Tutorial3/Containers/ShipContainer.cs
+++ Tutorial3/Tutorial3/Containers/ShipContainer.cs
@@ -2,5 +2,47 @@ namespace Tutorial3.Containers;
 
 public class ShipContainer
 {
+    public List<RefrigeratedContainer> ContainerTransport { get; set; }
+    public double MaxSpeed { get; set;}
+    public int MaxSpeedNumber { get; set;}
+    public double MaxSpeedWeight { get; set;}
     
+    public ShipContainer(double maxSpeed, int maxContainerNumber, double maxSpeedWeight) {
+        ContainerTransport = new List<RefrigeratedContainer>();
+        MaxSpeed = maxSpeed;
+        MaxSpeedNumber = maxContainerNumber;
+        MaxSpeedWeight = maxSpeedWeight;
+    }
+    
+    //Load cargo into a given container
+    public void LoadCargo(List<RefrigeratedContainer> container) {
+        foreach (var cargoload in container) {
+            if (ContainerTransport.Count >= MaxSpeedNumber)
+            {
+                throw new System.Exception("Error - cannot load more containers.");
+            }
+
+            ContainerTransport.Add(cargoload);
+        }
+    }
+    //Print information about a given container
+    public void PrintInfoContainer(RefrigeratedContainer container)
+    {
+        Console.WriteLine("Product Type - {container}.ProductType}};" +
+                          "Required Temperature - {{container.TemperatureRequiered}};" +
+                          "Current Temperature - {{container.TemperatureNow}}");
+    }
+    
+    // Print information about a given ship and its cargo
+    public void PrintInfoShip()
+    {
+        Console.WriteLine("Information about a ship: Max Speed - {MaxSpeed};" +
+                          "Max Container Number - {MaxSpeedNumber}; " +
+                          "Max Weight - {MaxSpeedWeight}");
+        Console.WriteLine("Information about the Containers:");
+        foreach (var container in ContainerTransport)
+        {
+            PrintInfoContainer(container);
+        }
+    }
 }
