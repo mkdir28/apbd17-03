cbc2fd4dea69bdcc31934a223a8eb3e91b149eeb
Maryna Kamienieva <mkdirwd@gmail.com>
2024-03-23 14:40:09 +0100

    created class Refrigerated container (C)

--- Tutorial3/Tutorial3/Containers/RefrigeratedContainer.cs
+++ Tutorial3/Tutorial3/Containers/RefrigeratedContainer.cs
@@ -2,5 +2,16 @@ namespace Tutorial3.Containers;
 
 public class RefrigeratedContainer
 {
+    public string ProductType { get; set; }
+    public double TemperatureNow { get; set; }
+    public double TemperatureRequiered { get; set; }
+
+    public RefrigeratedContainer(string productType, double temperatureNow,
+        double temperatureRequiered)
+    {
+        ProductType = productType;
+        TemperatureNow = TemperatureRequiered;
+        TemperatureRequiered = temperatureRequiered;
+    }
     
 }
